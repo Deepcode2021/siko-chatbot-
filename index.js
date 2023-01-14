5b11cf7f311e711c712e9aa67be0acb115abb09d
Deepcode2021 <91797541+Deepcode2021@users.noreply.github.com>
2023-01-14 11:39:44 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/chatbot', (req, res) => {
+  const { message } = req.body;
+  let response;
+
+  if (message.toLowerCase().includes('hello')) {
+    response = 'Hello there!';
+  } else if (message.toLowerCase().includes('weather')) {
+    response = 'The weather is nice today!';
+  } else {
+    response = 'I am not sure what you mean.';
+  }
+
+  res.json({ response });
+});
+
+app.listen(3000, () => {
+  console.log('Chatbot server listening on port 3000!');
+});
